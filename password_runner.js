ed4b04ff77dc48fdb2f83c3c8ba9912381fc23ff
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-08-26 19:02:57 +0200

    removed test files

--- password_runner.js
+++
@@ -1,9 +0,0 @@
-const exec = require('child_process').exec;
-const myShellScript = exec('passwordApply.sh /myDir');
-myShellScript.stdout.on('data', (data)=>{
-    console.info(data); 
-    // do whatever you want here with data
-});
-myShellScript.stderr.on('data', (data)=>{
-    console.error(data);
-});
